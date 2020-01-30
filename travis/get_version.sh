ba10bead8d6b9a6785019670957210c0bc59ac9c
stlouisn <stlouisn@hotmail.com>
2020-01-30 08:31:35 -0400

    Update get_version.sh

--- travis/get_version.sh
+++ travis/get_version.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-# Output version of nextcloud from nextcloud:apache docker
+# Output nextcloud version from nextcloud:apache docker
 echo "$((docker pull library/nextcloud:apache && docker run -it --rm library/nextcloud:apache /bin/bash -c env) | grep NEXTCLOUD_VERSION | awk -F '=' {'print $2'})"
